e9de85c828131fd8a19336c15b6e9a4adca890b4
Aidan Condon <78163010+aidancondon-BC@users.noreply.github.com>
2022-06-10 02:21:14 -0400

    Update model.py

--- model.py
+++ model.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from profile import Profile as prof
 from xgboost import XGBClassifier
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.metrics import mean_absolute_error
 
